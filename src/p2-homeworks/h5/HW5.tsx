bf4d57571af4f1825c5f44bb2704ed8334c6130a
m-marc <marchenkovmaxim@gmail.com>
2020-12-18 14:25:46 +0300

    Fixed style issues

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import Header from "./Header";
 import Routes from "./Routes";
 import {HashRouter} from "react-router-dom";
-import s from "../../p1-main/m1-ui/u1-app/App.module.css";
+import s from "./h5.module.css";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faAlignJustify} from "@fortawesome/free-solid-svg-icons";
 
@@ -10,7 +10,6 @@ function HW5() {
     const [navStatus, setNavStatus] = useState(false);
     return (
         <div>
-            {/*в gh-pages лучше работает HashRouter*/}
             <HashRouter>
                 <button className={s.btn} onClick={ () => setNavStatus(!navStatus) }>
                     <FontAwesomeIcon icon={faAlignJustify} size="2x" />
