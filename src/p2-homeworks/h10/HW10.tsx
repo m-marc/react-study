8c346dc3837b8ffda2fd88551bac2451629015ec
m-marc <marchenkovmaxim@gmail.com>
2021-01-18 18:37:45 +0300

    hw10

--- src/p2-homeworks/h10/HW10.tsx
+++ src/p2-homeworks/h10/HW10.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "./bll/store";
+import {loadingAC} from "./bll/loadingReducer";
+import s from "./hw10.module.css"
 
 function HW10() {
-    // useSelector, useDispatch
-    const loading = false;
+    const loading = useSelector((state: AppStoreType) => state.loading)
+    const dispatch = useDispatch()
 
     const setLoading = () => {
-        // dispatch
-        // setTimeout
+        dispatch(loadingAC(true))
+        setTimeout( () => {
+            dispatch(loadingAC(false))
+        }, 2000)
         console.log("loading...");
     };
 
@@ -19,7 +25,11 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <div className={s.spinnerWrapper}>
+                        <div className={s.spinnerInner}>
+                            <div className={s.spinner}>&nbsp;</div>
+                        </div>
+                    </div>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,20 @@
-const initState = {
+const initState = false;
 
-};
+export type showLoaderActionType = {
+    type: 'SHOW-LOADER',
+    isLoading: boolean
+}
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: showLoaderActionType): boolean => {
     switch (action.type) {
-        case "": {
+        case "SHOW-LOADER": {
+            state = action.isLoading
             return state;
         }
         default: return state;
     }
 };
 
-export const loadingAC = (): any => {}; // fix any
+export const loadingAC = (isLoading: boolean): showLoaderActionType => {
+    return {type: 'SHOW-LOADER', isLoading}
+};
