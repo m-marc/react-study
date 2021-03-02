0e6d16fc9c069b5e1e20095c3ba697424657e7be
m-marc <marchenkovmaxim@gmail.com>
2021-03-02 15:07:01 +0300

    Fixed axios intance

--- src/p2-homeworks/h13/RequestsAPI.ts
+++ src/p2-homeworks/h13/RequestsAPI.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://neko-cafe-back.herokuapp.com/auth/test',
-    // @ts-ignore
-    body: {success: true}
+    baseURL: 'https://neko-cafe-back.herokuapp.com/auth/test'
 })
 
 export const requestsAPI = {
